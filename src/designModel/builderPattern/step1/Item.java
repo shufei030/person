08d0319e6b3993721481a191871bc18235d4d0e5
shufei030 <shufei030@126.com>
2017-01-05 17:35:32 +0800

    创造者模式1

--- src/designModel/builderPattern/step1/Item.java
+++ src/designModel/builderPattern/step1/Item.java
@@ -3,7 +3,7 @@
 * @Package designModel.builderPattern.step1
 * @Description: 创建一个表示食物条目和食物包装的接口。
 * @author 舒飞
-* @date 2017-1-5 下午2:01:49 
+* @date 2017-1-5 下午6:01:49 
 */ 
 package designModel.builderPattern.step1;
 
